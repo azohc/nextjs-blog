8f6a82d09b926fc33c51ba1bee2ed1b6e8f4843e
jchozas <juanchozass@gmail.com>
2022-02-06 12:52:25 +0100

    fetching md posts

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import Head from "next/head"
+import Image from "next/image"
+import Link from "next/link"
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
-    return <h1>First Post</h1>
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <Image src="/images/profile.jpg" alt="Eyeball" height={359} width={444} />
+    </Layout>
+  )
 }
